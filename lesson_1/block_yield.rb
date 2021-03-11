245a80a9dd9402a029b1de638e8cdf566b2e7ffc
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-11 09:24:26 -0600

    update block_yield.rb with new example

--- lesson_1/block_yield.rb
+++ lesson_1/block_yield.rb
@@ -1,10 +1,12 @@
-# method implementation
-def say(words)
-  yield if block_given?
-  puts "> " + words
+# method definition
+def increment(number)
+  if block_given?
+    yield(number + 1)
+  end
+  number + 1
 end
 
 # method invocation
-say("hi there") do
-  system 'clear'
+increment(5) do |num|
+  puts num
 end
