1bd9ceec764f0224da335243bfd77b7aa5ffd768
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-11 09:53:56 -0600

    Add explicit block parameter example

---
+++ lesson_1/explicit_block_param.rb
@@ -0,0 +1,24 @@
+# def test(&block)
+#   puts "What's &block? #{block}"
+# end
+
+# test { "stringy" }
+
+def test2(block)
+  puts "hello"
+  block.call
+  puts "good-bye"
+end
+
+def test(&block)
+  puts "1"
+  test2(block)
+  puts "2"
+end
+
+test { puts "xyz" }
+# 1
+# hello
+# xyz
+# good-bye
+# 2
