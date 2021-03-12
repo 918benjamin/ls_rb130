d58d583a46861e9fb4d81a97ba8b7416605e7fff
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-12 09:34:52 -0600

    Add launch school walkthrough of each method

--- lesson_1/each.rb
+++ lesson_1/each.rb
@@ -1,16 +1,37 @@
 # My implementation (Pre-walkthrough)
-def each(arr)
+# def each(arr)
+#   counter = 0
+
+#   while counter < arr.size
+#     yield(arr[counter]) if block_given?
+#     counter += 1
+#   end
+
+#   arr
+# end
+
+# LS Implementation (walkthrough)
+def each(array)
   counter = 0
 
-  while counter < arr.size
-    yield(arr[counter]) if block_given?
+  while counter < array.size
+    yield(array[counter])
     counter += 1
   end
 
-  arr
+  array
 end
 
 
 # Each method invocation
-test = each([1, 2, 3]) { |num| puts num }
-p test
+# test = each([1, 2, 3]) { |num| puts num }
+# p test
+
+# puts ""
+
+# each([1, 2, 3, 4, 5]) do |num|
+#   puts num
+# end
+
+odds = each([1, 2, 3, 4, 5]) { |num| "do nothing" }.select { |num| num.odd? }
+puts odds
