3cfdf05dcce1d95fd298365de923391206432476
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-12 09:54:35 -0600

    Add LS solution to the select method

--- lesson_1/select.rb
+++ lesson_1/select.rb
@@ -1,15 +1,31 @@
 # My solution
+# def select(array)
+#   new_array = []
+#   counter = 0
+
+#   while counter < array.size
+#     block_return = yield(array[counter])
+#     new_array << array[counter] if block_return
+#     counter += 1
+#   end
+
+#   new_array
+# end
+
+# LS Solution
 def select(array)
-  new_array = []
   counter = 0
+  result = []
 
   while counter < array.size
-    block_return = yield(array[counter])
-    new_array << array[counter] if block_return
+    current_element = array[counter]
+    result << current_element if yield(current_element) # we don't need to assign
+    # the block return value to anything because we can simply reference it directly
+    # but this won't work for a Array#map clone
     counter += 1
   end
 
-  new_array
+  result
 end
 
 # Method invocation
