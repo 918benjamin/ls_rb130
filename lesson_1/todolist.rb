b315c4c4c2507e4f2d5d347701f2c5dfd28ebfeb
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-12 12:06:24 -0600

    Add my first solution of the TodoList class before checking the LS Solution

--- lesson_1/todolist.rb
+++ lesson_1/todolist.rb
@@ -49,7 +49,7 @@ end
 # puts todo2 # [] Clean room
 # puts todo3 # [] Go to gym
 
-class Todolist
+class TodoList
   attr_accessor :title
 
   def initialize(title)
@@ -57,8 +57,78 @@ class Todolist
     @todos = []
   end
 
-  # rest of the class needs implementation
+  def add(todo)
+    if todo.class == Todo
+      todos << todo
+    else
+      raise TypeError, "Can only add Todo objects"
+    end
+    todos
+  end
 
+  alias_method :<<, :add
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def to_a
+    todos.clone
+  end
+
+  def done?
+    todos.all? { |todo| todo.done? }
+  end
+
+  def item_at(index)
+    todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def done!
+    todos.each do |todo|
+      todo.done!
+    end
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    item_at(index)
+    todos.delete_at(index)
+  end
+
+  def to_s
+    puts " ---- #{title} ----"
+    todos.each do |todo|
+      puts todo
+    end
+  end
+
+  private
+
+  attr_accessor :todos
 end
 
 # Make this code work by implementing the rest of the class above
@@ -70,15 +140,15 @@ list = TodoList.new("Today's Todos")
 # ---- Adding to the list ----
 
 # add
-list.add(todo1)  # adds todo1 to the end of list, returns list
-list.add(todo2)  # same with todo2
-list.add(todo3)  # same with todo3
-list.add(1)      # raises TypeError with message "Can only add Todo objects"
+# list.add(todo1)  # adds todo1 to the end of list, returns list
+# list.add(todo2)  # same with todo2
+# list.add(todo3)  # same with todo3
+# list.add(1)      # raises TypeError with message "Can only add Todo objects"
 
 # << should implement the same behavior as add
-# list << todo1    # adds todo1 to the end of list, returns list
-# list << todo2    # same with todo2
-# list << todo3    # same with todo3
+list << todo1    # adds todo1 to the end of list, returns list
+list << todo2    # same with todo2
+list << todo3    # same with todo3
 # list << 1        # raises TypeError with message "Can only add Todo objects"
 
 # ---- Interrogating the list ---
@@ -101,15 +171,15 @@ list.add(1)      # raises TypeError with message "Can only add Todo objects"
 # ---- Retrieving an item in the list ----
 
 # item_at
-# list.item_at       # raises ArgumentError
-# p list.item(1)       # returns 2nd item in list (zero based index)
+# list.item_at         # raises ArgumentError
+# p list.item_at(1)    # returns 2nd item in list (zero based index)
 # p list.item_at(100)  # raises IndexError
 
 # ---- Marking items in the list ----
 
 # mark_done_at
 # list.mark_done_at               # raises ArgumentError
-# p list.mark_done_at(1)            # marks the 2nd item as done
+# p list.mark_done_at(1)          # marks the 2nd item as done
 # list.mark_done_at(100)          # raises IndexError
 
 # mark_undone_at
@@ -120,23 +190,24 @@ list.add(1)      # raises TypeError with message "Can only add Todo objects"
 # done!
 # list.done!                      # marks all items as done
 
+
 # ---- Deleting from the list -----
 
 # shift
-# list.shift                      # removes and returns the first item in list
+# p list.shift                      # removes and returns the first item in list
 
 # pop
-# list.pop                        # removes and returns the last item in list
+# p list.pop                        # removes and returns the last item in list
 
 # remove_at
 # list.remove_at                  # raises ArgumentError
-# list.remove_at(1)               # removes and returns the 2nd item
+# p list.remove_at(1)               # removes and returns the 2nd item
 # list.remove_at(100)             # raises IndexError
 
 # ---- Outputting the list -----
 
 # to_s
-# list.to_s                      # returns string representation of the list
+list.to_s                      # returns string representation of the list
 
 # ---- Today's Todos ----
 # [ ] Buy milk
