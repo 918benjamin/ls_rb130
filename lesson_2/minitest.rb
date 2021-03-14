492a17130348ee099befd05e368fef0cf90d9dbb
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-14 12:58:05 -0500

    Add second test case

--- lesson_2/minitest.rb
+++ lesson_2/minitest.rb
@@ -3,12 +3,17 @@ require 'minitest/autorun'
 # MiniTest::Reporters.use!
 
 def square_root(value)
-  Math.sqrt(value)
+  Math.sqrt(value).to_i
 end
 
 class SquareRootTest < MiniTest::Test
   def test_that_square_root_of_9_is_3
     result = square_root(9)
     assert_equal 3, result
+  end
+
+  def test_that_square_root_of_17_is_4
+    result = square_root(17)
+    assert_equal 4, result
   end
 end
