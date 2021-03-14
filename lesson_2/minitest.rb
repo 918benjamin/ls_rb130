65886cff111256f1dd570be4f6a9ba2af37ae76c
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-14 12:55:25 -0500

    Add minitest example that fails"

---
+++ lesson_2/minitest.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+# require 'minitest/reporters'
+# MiniTest::Reporters.use!
+
+def square_root(value)
+end
+
+class SquareRootTest < MiniTest::Test
+  def test_that_square_root_of_9_is_3
+    result = square_root(9)
+    assert_equal 3, result
+  end
+end
